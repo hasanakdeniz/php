813fcd4ac963277497782db6babade4bc1e4401a
HA <hasanakdeniz@outlook.com>
2025-10-02 19:02:38 +0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -6,7 +6,7 @@
     <title>sudosu örnek php</title>
 </head>
 <body>
-    <h1>Merhaba Dünya, Merhaba sudosu!</h1>
+    <h1>Merhaba Dünya, Ben Dost! Merhaba sudosu!</h1>
     <p><a href="/info.php">Buradan</a>, dokploy'da deploy etmek üzere sudosu için oluşturulmuş php versiyonunu inceleyebilirsin.</p>
     <p><?php echo"php :)"; ?></p>
 </body>
