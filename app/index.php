a12f1505dbe1569081364d7f4c7323e863c72b22
HA <hasanakdeniz@outlook.com>
2025-10-02 19:43:38 +0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>sudosu örnek php</title>
+    <title><?php echo explode('.', $_SERVER['HTTP_HOST'])[0]; ?> örnek php</title>
 </head>
 <body>
-    <h1>Merhaba Dünya, Ben Dost! Merhaba sudosu!</h1>
-    <p><a href="/info.php">Buradan</a>, dokploy'da deploy etmek üzere sudosu için oluşturulmuş php versiyonunu inceleyebilirsin.</p>
+    <h1>Merhaba Dünya, Ben Dost! Merhaba <?php echo explode('.', $_SERVER['HTTP_HOST'])[0]; ?>!</h1>
+    <p><a href="/info.php">Buradan</a>, dokploy'da deploy etmek üzere <?php echo explode('.', $_SERVER['HTTP_HOST'])[0]; ?> için oluşturulmuş php versiyonunu inceleyebilirsin.</p>
     <p><?php echo"php :)"; ?></p>
 </body>
 </html>
