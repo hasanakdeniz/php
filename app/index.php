8f4f4f03bff4943f0c66081caea411f489687653
HA <hasanakdeniz@outlook.com>
2025-10-02 14:46:19 +0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,3 +1,13 @@
-<?php
-echo phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sudosu örnek php</title>
+</head>
+<body>
+    <h1>Merhaba Dünya, Merhaba sudosu!</h1>
+    <p><a href="/info.php">Buradan</a>, dokploy'da deploy etmek üzere sudosu için oluşturulmuş php versiyonunu inceleyebilirsin.</p>
+    <p><?php echo"php :)"; ?></p>
+</body>
+</html>
