9eecd7a966a0af57337a10ca702593a26f9e7e52
HA <hasanakdeniz@outlook.com>
2025-10-03 12:50:33 +0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo explode('.', $_SERVER['HTTP_HOST'])[0]; ?> örnek php</title>
+    <title>Merhaba <?php echo explode('.', $_SERVER['HTTP_HOST'])[0]; ?></title>
 </head>
 <body>
-    <h1>Merhaba Dünya, Ben Dost! Merhaba <?php echo explode('.', $_SERVER['HTTP_HOST'])[0]; ?>!</h1>
-    <p><a href="/info.php">Buradan</a>, dokploy'da deploy etmek üzere <?php echo explode('.', $_SERVER['HTTP_HOST'])[0]; ?> için oluşturulmuş php versiyonunu inceleyebilirsin.</p>
+    <h1>Merhaba Dünya, Merhaba <?php echo explode('.', $_SERVER['HTTP_HOST'])[0]; ?>!</h1>
+    <p><a href="/info.php">Buradan</a>, <a href="https://dokploy.com/" target="_new">dokploy</a>'da deploy etmek üzere <?php echo explode('.', $_SERVER['HTTP_HOST'])[0]; ?> için oluşturulmuş php versiyonunu inceleyebilirsin.</p>
+    <p><a href="https://sudosu.tr/" target="_new">sudosu!</a></p>
     <p><?php echo"php :)"; ?></p>
 </body>
 </html>
