6ae0c7cbbb18231abfd27deb37af8cc94cf64f51
HA <hasanakdeniz@outlook.com>
2025-10-16 00:16:21 +0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title id="htmlTitle">SITEMIZ - Bakım Modu</title>
+    <title id="htmlTitle">Bakım Modu</title>
     <style>
         * {
             margin: 0;
